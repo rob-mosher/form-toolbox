6f3a76a2ea4995da0519b4d097c6328f1672792d
Rob Mosher <rob@robmosher.com>
2023-11-18 19:29:45 -0500

    wip

--- src/api/services/aws/sqs/messageProcessor.js
+++ src/api/services/aws/sqs/messageProcessor.js
@@ -1,18 +1,67 @@
+require('dotenv').config();
+
+const { fromEnv } = require('@aws-sdk/credential-providers');
 const { Op } = require('sequelize');
+const { SQSClient, SendMessageCommand, DeleteMessageCommand } = require('@aws-sdk/client-sqs');
 
 const s3Functions = require('../s3/s3Functions');
 const { Form } = require('../../../models');
 
+const REQUEUE_DELAY = 30; // in seconds
+const REQUEUE_MAX_ATTEMPTS = 5;
+
+const {
+  AWS_ACCOUNT_NUMBER,
+  AWS_QUEUE_NAME,
+  AWS_REGION,
+} = process.env;
+
+const queueURL = `https://sqs.${AWS_REGION}.amazonaws.com/${AWS_ACCOUNT_NUMBER}/${AWS_QUEUE_NAME}`;
+console.log('queueURL:', queueURL);
+
+const sqsClient = new SQSClient({
+  region: AWS_REGION,
+  credentials: fromEnv(),
+});
+
 const processMessage = async (mes) => {
   console.log('Processing message: ', mes.Body);
+  console.log('Receipt handle: ', mes.ReceiptHandle);
 
-  const parsedBody = JSON.parse(mes.Body);
+  // const parsedBody = JSON.parse(mes.Body);
+  // const parsedMessageContent = JSON.parse(parsedBody.Message);
+
+  let parsedBody;
+  try {
+    parsedBody = JSON.parse(mes.Body);
+  } catch (e) {
+    console.error('Error parsing message body:', mes.Body, e);
+    return; // Stop processing if the body can't be parsed
+  }
+  console.log('parsedBody', parsedBody);
+
+  // Check if parsedBody.Message exists and is a string
+  if (typeof parsedBody.Message !== 'string') {
+    console.error('parsedBody.Message is not a string:', parsedBody.Message);
+    return; // Stop processing if Message is not a string
+  }
+
+  let parsedMessageContent;
+  try {
+    parsedMessageContent = JSON.parse(parsedBody.Message);
+  } catch (e) {
+    console.error('Error parsing parsedBody.Message:', parsedBody.Message, e);
+    return; // Stop processing if the message content can't be parsed
+  }
+
+  parsedMessageContent.AttemptCount = (parsedMessageContent.AttemptCount || 0) + 1;
+
+  // NOTE AWS textract is limited in how it can be structured, so "JobId" is "textractJobId"
   const {
-    // NOTE AWS textract is limited in how it can be structured, so "JobId" is "textractJobId"
     JobId: textractJobId,
     Status: status,
     FormId: formId,
-  } = JSON.parse(parsedBody.Message); // NOTE this is how AWS textract forms their data
+  } = parsedMessageContent;
 
   switch (status) {
     case 'ANALYZING': {
@@ -66,7 +115,13 @@ const processMessage = async (mes) => {
       });
 
       if (!form) {
-        console.warn(`No form found with textractJobId ${textractJobId}. A possible race condition occured where the analysis results were queued before the notice of them starting (which writes the textract job id).`);
+        if (parsedMessageContent.AttemptCount <= REQUEUE_MAX_ATTEMPTS) {
+          await requeueMessage(mes.ReceiptHandle, parsedMessageContent);
+          console.warn(`Requeued message for textractJobId ${textractJobId}. Attempt count: ${parsedMessageContent.AttemptCount}`);
+        } else {
+          console.error(`Max retries reached for message: ${mes.MessageId}`);
+          // Handle max retries (e.g., log, move to DLQ)
+        }
         break;
       }
 
@@ -91,6 +146,39 @@ const processMessage = async (mes) => {
 
     default: {
       console.log(`No matching action for status ${status}`);
+    }
+  }
+
+  async function requeueMessage(receiptHandle, msgContent) {
+    const newMsgContent = {
+      ...msgContent,
+      AttemptCount: (msgContent.AttemptCount) + 1, // use TitleCase here to match AWS
+    };
+
+    console.log('newMsgContent', newMsgContent);
+
+    // Prepare new message parameters
+    const backoffDelaySeconds = REQUEUE_DELAY * (newMsgContent.AttemptCount);
+    const sendMessageParams = {
+      QueueUrl: queueURL,
+      MessageBody: JSON.stringify(newMsgContent),
+      DelaySeconds: backoffDelaySeconds,
+    };
+
+    try {
+      // Send the new message
+      await sqsClient.send(new SendMessageCommand(sendMessageParams));
+      console.log('Message re-queued successfully');
+
+      // Delete the original message
+      const deleteMessageParams = {
+        QueueUrl: queueURL,
+        ReceiptHandle: receiptHandle,
+      };
+      await sqsClient.send(new DeleteMessageCommand(deleteMessageParams));
+      console.log('Original message deleted successfully');
+    } catch (error) {
+      console.error('Error in re-queuing the message:', error);
     }
   }
 };
